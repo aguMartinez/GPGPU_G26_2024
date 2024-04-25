34f7dc5f1bad78d2c3d17b4c7812c249346299e3
aguM-PC <agustin.martinez.acuna@fing.edu.uy>
2024-04-25 20:13:18 -0300

    ej1 Done

--- practico3/ej1.cu
+++ practico3/ej1.cu
@@ -6,8 +6,7 @@
 
 #define CUDA_CHK(ans) { gpuAssert((ans), __FILE__, __LINE__); }
 
-inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
-{
+inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true){
    if (code != cudaSuccess) 
    {
       fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
@@ -16,7 +15,7 @@ inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=t
 }
 
 int* randomMatrix(int n, int m){
-    int* A = (int*) malloc(n * m *sizeof(int*));
+    int* A = (int*) malloc(n * m *sizeof(int));
 
     for (int i = 0; i < n * m; i++) {
         A[i] = rand();
@@ -24,12 +23,21 @@ int* randomMatrix(int n, int m){
     return A;
 }
 
-__global__ void transpose_kernel(int* d_M, int* d_MTrans, int n){
-    int x = blockIdx.x * blockDim.x + threadIdx.x;
-    int y = blockIdx.y * blockDim.y + threadIdx.y
+void printMatrix(int* A, int n, int m){
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            printf("%d ", A[i * m + j]);
+        }
+        printf("\n");
+    }
+}
 
-    if (x < n && y < n){
-        d_MTrans[y + x * n] = d_M[x + y * n];
+__global__ void transposeKernel(int* d_M, int* d_MTrans, int n, int m){
+    int x = blockIdx.x * blockDim.x + threadIdx.x;
+    int y = blockIdx.y * blockDim.y + threadIdx.y;
+
+    if (x < n && y < m){
+        d_MTrans[y + x * n] = d_M[x + y * m];
     }
 }
 
@@ -37,15 +45,13 @@ __global__ void transpose_kernel(int* d_M, int* d_MTrans, int n){
  int main(){
 
     /* definir tamanios de matriz*/
-    int n = 256;
+    int n = 64;
     int m = n;
-    int size = n*m*sizeof(int)
+    int size = n*m*sizeof(int);
     
-    int* R;
-
-    /* generar matriz*/
-    int* h_M;
-    h_M = randomMatrix(n,m);
+    /* generar matrices del host */
+    int* h_M = randomMatrix(n,m);
+    int* h_MTrans = (int*)malloc(size);
 
     /* reservar memoria en la GPU */
     int* d_M;
@@ -54,16 +60,28 @@ __global__ void transpose_kernel(int* d_M, int* d_MTrans, int n){
     CUDA_CHK(cudaMalloc((void **)&d_MTrans, size)); //matriz de salida
 
 	/* copiar los datos de entrada a la GPU */
-
  	cudaMemcpy(d_M, h_M, size, cudaMemcpyHostToDevice);
 
     /* Ej 1:*/
-	int threads_per_block = 32*32;
-    int blocks_per_grid = 128; //Todo: arreglar este num.
-
-    transpose_kernel<<<blocks_per_grid, threads_per_block>>>(d_M, d_MTrans size);
+	dim3 threadsPerBlock(32,32);
+    dim3 numBlocks((m + threadsPerBlock.x - 1) / threadsPerBlock.x,
+                   (n + threadsPerBlock.y - 1) / threadsPerBlock.y);
+    transposeKernel<<<numBlocks, threadsPerBlock>>>(d_M, d_MTrans, n,m);
     
     /* Copiar los datos de salida a la CPU en h_message */
  	cudaMemcpy(h_MTrans, d_MTrans, size, cudaMemcpyDeviceToHost);
+
+    /* Imprimir resultados*/
+    printMatrix(h_M, n, m);
+    printf("-----------------------\n");
+    printMatrix(h_MTrans, n, m);
+
+    /* Liberar memoria */
+    free(h_M);
+    free(h_MTrans);
+    cudaFree(d_M);
+    cudaFree(d_MTrans);
+
     return 1;
- }
+}
+
