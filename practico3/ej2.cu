46813ac7b9a7f900cd94a7a9c4064a9639538490
aguM-PC <agustin.martinez.acuna@fing.edu.uy>
2024-04-25 23:50:16 -0300

    ej2 Done

--- practico3/ej2.cu
+++ practico3/ej2.cu
@@ -1,4 +1,4 @@
-/* Ejercicio 2 | Practico 3 | Grupo 26 | GPGPU 2024*/
+/* Ejercicio 2 | Practico 3 | Grupo 26 | GPGPU 2024 */
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -6,8 +6,7 @@
 
 #define CUDA_CHK(ans) { gpuAssert((ans), __FILE__, __LINE__); }
 
-inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
-{
+inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true){
    if (code != cudaSuccess) 
    {
       fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
@@ -16,53 +15,72 @@ inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=t
 }
 
 int* randomMatrix(int n, int m){
-    int* A = (int*) malloc(n * m *sizeof(int*));
+    int* A = (int*) malloc(n * m *sizeof(int));
 
     for (int i = 0; i < n * m; i++) {
-        A[i] = rand();
+        A[i] = rand() % 100;
     }
     return A;
 }
 
-
-__global__ void sum4thPosition_kernel(int* d_M, int* d_MRes){
-    int x = blockIdx.x * blockDim.x + threadIdx.x;
-    int y = blockIdx.y * blockDim.y + threadIdx.y
-
-    if (x < n && y < n && x+4 < n){
-        d_MRes[y + x * n] += d_M[4*x + y * n]; //probablemente con atomic add
+void printMatrix(int* A, int n, int m){
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            printf("%d ", A[i * m + j]);
+        }
+        printf("\n");
     }
 }
 
+__global__ void sum4thPosition_kernel(int* d_M, int* d_MRes, int n, int m){
+    int x = blockIdx.x * blockDim.x + threadIdx.x;
+    int y = blockIdx.y * blockDim.y + threadIdx.y;
 
- int main(){
+    if (x+4 < n && y < m){
+        d_MRes[x + y * n] = d_M[x + y * n] + d_M[(x+4) + y * n];
+    }
+}
+
+int main(){
 
     /* definir tamanios de matriz*/
     int n = 256;
     int m = n;
-    int size = n*m*sizeof(int)
-    
-    int* R;
+    int size = n*m*sizeof(int);
 
     /* generar matriz*/
-    int* h_M;
-    h_M = randomMatrix(n,m);
+    int* h_M = randomMatrix(n,m);
+    int* h_MRes = (int*)malloc(size);
 
     /* reservar memoria en la GPU */
+    int* d_M;
+    int* d_MRes;
 	CUDA_CHK(cudaMalloc((void **)&d_M, size)); //matriz de entrada
-    CUDA_CHK(cudaMalloc((void **)&d_MTrans, size)); //matriz de salida
+    CUDA_CHK(cudaMalloc((void **)&d_MRes, size)); //matriz de salida
 
 	/* copiar los datos de entrada a la GPU */
-    int* d_M;
  	cudaMemcpy(d_M, h_M, size, cudaMemcpyHostToDevice);
 
-    /* Ej 2:*/
-	int threads_per_block = 32*32;
-    int blocks_per_grid = 128; //Todo: arreglar este num.
+    /* Ej 2a */
+	dim3 threadsPerBlock(32,32);
+    dim3 numBlocks((m + threadsPerBlock.x - 1) / threadsPerBlock.x,
+                   (n + threadsPerBlock.y - 1) / threadsPerBlock.y);
 
-    sum4thPosition_kernel<<<blocks_per_grid, threads_per_block>>>(d_M, size);
-    
-    /* Copiar los datos de salida a la CPU en h_message */
- 	cudaMemcpy(h_MTrans, d_MTrans, size, cudaMemcpyDeviceToHost);
+    sum4thPosition_kernel<<<numBlocks, threadsPerBlock>>>(d_M, d_MRes, n, m);
+
+    /* Copiar los datos de salida a la CPU */
+ 	cudaMemcpy(h_MRes, d_MRes, size, cudaMemcpyDeviceToHost);
+
+    /* Imprimir resultados*/
+    printMatrix(h_M, n, m);
+    printf("-----------------------\n");
+    printMatrix(h_MRes, n, m);
+
+    /* Liberar memoria */
+    free(h_M);
+    free(h_MRes);
+    cudaFree(d_M);
+    cudaFree(d_MRes);
+
     return 1;
  }
